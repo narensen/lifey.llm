a1797fa52f0ab51503540df84c4a59102869c03e
Naren <106871870+narensen@users.noreply.github.com>
2024-10-01 23:08:59 +0530

    Update lifey.py

--- lifey.py
+++ lifey.py
@@ -31,7 +31,7 @@ if 'conversation_history' not in st.session_state:
     st.session_state.conversation_history = []
 
 # User input field for API key
-gemini_api_key = "AIzaSyCM0tK3ljTw79tuMx_s4-afMxmOqNwPGRc"
+gemini_api_key = #Enter Gemini API key
 
 if gemini_api_key:
     # Initialize Gemini chat object
